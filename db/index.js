221faf779534c3acf19f9841345a293e54d7a0fa
Brian Ko <brianko42@gmail.com>
2022-10-02 15:17:20 -0700

    heroku addin

--- db/index.js
+++ db/index.js
@@ -1,7 +1,7 @@
 const { Client } = require('pg'); // imports the pg module
 
 // supply the db name and location of the database
-const client = new Client('postgres://localhost:5432/juicebox-dev');
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/juicebox-dev');
 
 
 async function createUser({ 
@@ -267,7 +267,7 @@ async function getAllUsers() {
           message: "Could not find a post with that postId"
         };
       }
-      
+
       const { rows: tags } = await client.query(`
         SELECT tags.*
         FROM tags
